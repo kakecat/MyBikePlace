a879a2882504bb71f32f4f8eeaa40adadf7c7601
kakecat <125948218+kakecat>
2023-05-21 06:48:06 +0000

    修正2

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2023_05_20_044912) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.integer "user_id", default: 0, null: false
+    t.integer "post_id", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
